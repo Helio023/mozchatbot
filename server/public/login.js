39dd8d40de0218960b606b65692429ce1907c166
unknown <helioengracialipinze@gmail.com>
2023-05-06 05:15:57 -0700

    Login page finished

--- server/public/login.js
+++ server/public/login.js
@@ -1,10 +1,30 @@
 const form = document.querySelector('#form');
+const btn = document.querySelector('#btn');
+
+function hideAlert() {
+  const el = document.querySelector('.alert');
+  if (el) {
+    el.parentElement.removeChild(el);
+  }
+}
+
+function showAlert(type, msg) {
+  hideAlert();
+  const markup = `<div style="text-align: center" class='alert alert--${type}'>${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+
+  setTimeout(() => hideAlert(), 5000);
+}
 
 async function login(email, password) {
+  const production_url = 'https://mozbotchat.onrender.com/login';
+  // const dev_url = 'http://localhost:3000/login';
+  let isLoading;
   try {
+    isLoading = true;
     const res = await axios({
       method: 'POST',
-      url: 'https://mozbotchat.onrender.com/login',
+      url: production_url,
       data: {
         email,
         password,
@@ -12,13 +32,18 @@ async function login(email, password) {
     });
 
     if (res.data.status === 'success') {
+      isLoading = false;
+      showAlert('success', 'Usuário logado com sucesso!');
       setTimeout(() => {
         window.location.assign('/chat');
       }, 3000);
     }
   } catch (error) {
-    console.log(error);
+    isLoading = false;
+    showAlert('error', error.response.data);
   }
+
+  btn.textContent = `${isLoading ? 'Entrando...' : 'Entrar'}`;
 }
 
 form.addEventListener('submit', (e) => {
