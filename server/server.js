b5dc24f31b35ad17d5cec52e1948a2d8061df4c7
unknown <helioengracialipinze@gmail.com>
2023-05-14 13:43:01 -0700

    Custom domain added

--- server/server.js
+++ server/server.js
@@ -24,13 +24,6 @@ app.use(cors({
   origin: 'https://www.mozbotchat.com'
 }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.mozbotchat.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 app.use(express.json({limit: '10kb'}));
 app.use(express.urlencoded({ extended: false }));
