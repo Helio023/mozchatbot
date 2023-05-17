c2911318a05d59397f478f3a45828a93a4d05bdc
unknown <helioengracialipinze@gmail.com>
2023-05-17 13:08:52 -0700

    Afiliate logic finised in the backend

--- server/public/register.js
+++ server/public/register.js
@@ -20,30 +20,37 @@ const url = 'https://www.mozbotchat.com';
 // const url = 'http://localhost:3000';
 
 const register = (email, password, username, passwordConfirm) => {
+  const queryParams = new URLSearchParams(window.location.search);
+  const inviter = queryParams.get('inviter');
+
   btn.textContent = 'Registando...';
+
+  let requestUrl = `${url}/signup`; 
+
+  if (inviter) {
+    requestUrl += `?inviter=${inviter}`; 
+  }
 
   axios({
     method: 'POST',
-    url: `${url}/signup`,
+    url: requestUrl,
     data: {
       email,
       password,
       username,
-      passwordConfirm
+      passwordConfirm,
     },
   })
     .then(() => {
-     
-      btn.textContent = "Registar"
+      btn.textContent = 'Registar';
       showAlert('success', 'Usuário registado com sucesso!');
       setTimeout(() => {
         window.location.assign('/chat');
       }, 3000);
     })
     .catch((error) => {
-      btn.textContent = 'Registar'
-      showAlert('error', error.response?.data?.message?.split('. ')[0])
-      
+      btn.textContent = 'Registar';
+      showAlert('error', error.response?.data?.message?.split('. ')[0]);
     });
 };
 
@@ -51,8 +58,8 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const email = document.querySelector('#email');
   const password = document.querySelector('#password');
-  const passwordConfirm = document.querySelector('#password-confirm')
-  const username = document.querySelector('#username')
+  const passwordConfirm = document.querySelector('#password-confirm');
+  const username = document.querySelector('#username');
 
   register(email.value, password.value, username.value, passwordConfirm.value);
 });
