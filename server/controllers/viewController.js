d0a0c33e09f1e4bef4fa28af6f5aa67b10e0777e
Helio Engrácia Mapupo <helioengracialipinze@gmail.com>
2023-07-03 11:50:42 +0100

    JWT error fixed

--- server/controllers/viewController.js
+++ server/controllers/viewController.js
@@ -19,11 +19,11 @@ exports.home = catchAsyncError(async (req, res, next) => {
     const currentUser = await User.findById(decoded.id);
 
     if (!currentUser) {
-      return next();
+      return res.status(200).redirect('login');
     }
 
     if (currentUser.changedPasswordAfterJWT(decoded.iat)) {
-      return next();
+      return res.status(200).redirect('login');
     }
 
     return res.status(200).redirect('chat');
@@ -31,6 +31,37 @@ exports.home = catchAsyncError(async (req, res, next) => {
 
   return res.status(200).render('login');
 });
+
+// exports.home = catchAsyncError(async (req, res, next) => {
+//   if (req.cookies.jwt && req.cookies.jwt !== 'loggedout') {
+//     const promisifyDecoded = promisify(jwt.verify);
+
+//     try {
+//       const decoded = await promisifyDecoded(
+//         req.cookies.jwt,
+//         process.env.JWT_SECRET
+//       );
+
+//       const currentUser = await User.findById(decoded.id);
+
+//       if (!currentUser) {
+//         return next();
+//       }
+
+//       if (currentUser.changedPasswordAfterJWT(decoded.iat)) {
+//         return next();
+//       }
+
+//       return res.status(200).redirect('chat');
+//     } catch (err) {
+//       // Erro ao verificar o token JWT (expirado, inválido, etc.)
+//       return res.status(200).redirect('login');
+//     }
+//   }
+
+//   return res.status(200).render('login');
+// });
+
 
 exports.settings = catchAsyncError(async (req, res, next) => {
   if (req.cookies.jwt && req.cookies.jwt !== 'loggedout') {
